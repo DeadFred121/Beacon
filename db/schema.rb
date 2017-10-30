ed1488398391155d112f85908615c6fd6278332d
DeadFred <f.cliff1@hotmail.com>
2017-10-30 10:47:07 +1000

    Region and Platform models created and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030003724) do
+ActiveRecord::Schema.define(version: 20171030004525) do
+
+  create_table "platforms", force: :cascade do |t|
+    t.string "platform"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "regions", force: :cascade do |t|
+    t.string "region"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
