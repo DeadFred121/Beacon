11f3d5beb253b1c6bf942daa2ab4e0a914ed04a9
DeadFred <f.cliff1@hotmail.com>
2017-11-03 11:11:25 +1000

    basic Stripe checkout implementation, custom amount and description modules. Payment can be configured to be accepted in Bitcoin too. Payment also uses the user's email by default.

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,35 @@
+class ChargesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :amount_to_be_charged
+  before_action :set_description
+
+  def new
+  end
+
+  def create
+    customer = StripeTool.create_customer(email: params[:stripeEmail],
+                                          stripe_token: params[:stripeToken])
+
+    charge = StripeTool.create_charge(customer_id: customer.id,
+                                      amount: @amount,
+                                      description: @description)
+
+    redirect_to thanks_path
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
+  end
+
+  def thanks
+  end
+
+    private
+
+      def amount_to_be_charged
+        @amount = 500
+      end
+
+      def set_description
+        @description = "Beacon Pro Upgrade"
+      end
+end
