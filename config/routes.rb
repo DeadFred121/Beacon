103795f582501897fed1fa330ff1720c0001f5c9
DeadFred <f.cliff1@hotmail.com>
2017-11-07 16:36:59 +1000

    updated routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "users/registrations" }
   resources :users, :only => [:show]
 
-
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
